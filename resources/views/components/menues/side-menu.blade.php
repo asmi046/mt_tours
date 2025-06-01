9289e8b6127aff1a799575124b81507b52614b89
asmi046 <asmi046@gmail.com>
2025-06-01 17:52:15 +0300

    +ссылка на туры

--- resources/views/components/menues/side-menu.blade.php
+++ resources/views/components/menues/side-menu.blade.php
@@ -18,7 +18,7 @@
     <div class="comment">Календарь туров
         <span class="arrow"></span>
     </div>
-    <a href="https://www.mirturizma46.ru/kalendar-turov" class="calendat_lnk button-circle">
+    <a href="{{ route('calendar') }}" class="calendat_lnk button-circle">
         <svg class="sprite_icon">
             <use xlink:href="#calendar"></use>
         </svg>
