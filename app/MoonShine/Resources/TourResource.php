3cac6ea05b9e5c51c187fc79389d8ca9b89ab3d5
asmi046 <asmi046@gmail.com>
2025-06-13 16:39:41 +0300

    - required

--- app/MoonShine/Resources/TourResource.php
+++ app/MoonShine/Resources/TourResource.php
@@ -72,7 +72,7 @@ class TourResource extends ModelResource
                     Switcher::make('Актуальный тур', 'actual'),
                     Number::make('Порядок', 'order'),
                     Url::make('Внешняя ссылка', 'out_lnk'),
-                    Text::make('Шаблон', 'template_name')->required(),
+                    Text::make('Шаблон', 'template_name'),
                     Flex::make([
                         Text::make('Заголовок (h1)', 'title')->required(),
                         Text::make('Заголовок на карточке', 'title_card'),
