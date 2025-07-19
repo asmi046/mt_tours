a25c82e637cc45eaebcfd3123d418c077eb4dd8b
asmi046 <asmi046@gmail.com>
2025-07-19 22:55:31 +0300

    Адыгея вместо Волгограда

--- app/View/Components/BannerToursPrev.php
+++ app/View/Components/BannerToursPrev.php
@@ -15,7 +15,7 @@ class BannerToursPrev extends Component
      */
     public function __construct()
     {
-        $this->tours = Tour::whereIn('id', [11, 7, 8, 9])->get();
+        $this->tours = Tour::whereIn('id', [10, 7, 8, 9])->get();
     }
 
     /**
