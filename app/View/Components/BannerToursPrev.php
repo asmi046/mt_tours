81ed3970e02931430b7de6255e8bb03fbd9da1cd
asmi046 <asmi046@gmail.com>
2025-08-05 17:35:06 +0300

    Казань на нужное место

--- app/View/Components/BannerToursPrev.php
+++ app/View/Components/BannerToursPrev.php
@@ -15,7 +15,15 @@ class BannerToursPrev extends Component
      */
     public function __construct()
     {
-        $this->tours = Tour::whereIn('id', [11, 10, 8, 9])->get();
+
+        $this->tours = collect([
+            Tour::where('id', 11)->first(),
+            Tour::where('id', 10)->first(),
+            Tour::where('id', 9)->first(),
+            Tour::where('id', 6)->first(),
+        ])->filter();
+
+        // $this->tours = Tour::whereIn('id', [10, 9, 11])->get();
         // $orderedItems = $items->sortBy(function ($item) use ($ids) {
         //     return array_search($item->id, $ids);
         // })->values();
