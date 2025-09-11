4c79fbf5b17f9a9cc3d292c2a92bc3f9aee8360d
asmi046 <asmi046@gmail.com>
2025-09-11 18:33:06 +0300

    Сррелка для писем

--- resources/views/mail/pay/client-pay-success.blade.php
+++ resources/views/mail/pay/client-pay-success.blade.php
@@ -4,7 +4,7 @@
     <h1>🎉 Поздравляем с покупкой 🎉</h1>
     <p>Вы приобрели:</p>
     <h3>{{ $pay->name }}</h3>
-    <img class="zig_arrow" src="{{ asset('img/arrows/zig-arrow.png') }}" alt="Перейти к билетам">
+    <img class="zig_arrow" src="{{ asset('img/ticket/zig-arrow.png') }}" alt="Перейти к билетам">
     <br>
     <br>
     <a href="{{ route('show_ticket', ['uuid' => $pay->uuid]) }}" class="button">Скачать билеты</a>
