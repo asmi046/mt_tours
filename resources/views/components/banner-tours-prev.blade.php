b347b4422bd5b14069f1a04104d748864fd49de8
asmi046 <asmi046@gmail.com>
2025-09-21 23:37:02 +0300

    +1

--- resources/views/components/banner-tours-prev.blade.php
+++ resources/views/components/banner-tours-prev.blade.php
@@ -9,7 +9,7 @@
     </div>
 </a> --}}
 
-<a href="{{ route("tour_page", 'new-year') }}" class="item new_year_item">
+<a href="{{ route("tour_category", 'new-year') }}" class="item new_year_item">
     <img src="{{ asset('img/new_year/new_head_banner.webp') }}" alt="Новогодние туры от МирТуризма">
     <div class="overlay"></div>
     <div class="text">
--- resources/views/components/menu-puncts.blade.php
+++ resources/views/components/menu-puncts.blade.php
@@ -1,5 +1,5 @@
 <ul class="menue_puncts_list">
-    <li><a class="yakor" href="{{ route("tour_page", 'new-year') }}"><span class="emoji">🎄</span>Новогодние туры</a></li>
+    <li><a class="yakor" href="{{ route("tour_category", 'new-year') }}"><span class="emoji">🎄</span>Новогодние туры</a></li>
     <li><a class="yakor" href="{{ route('home') }}">Главная страница</a></li>
     <li><a class="yakor" href="#contacts">Контакты</a></li>
 </ul>
