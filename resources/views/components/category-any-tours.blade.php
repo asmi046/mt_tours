ccd55d2196c595fb6e0fdc075d0b92cd64b5260c
asmi046 <asmi046@gmail.com>
2025-10-09 00:54:51 +0300

    +1

--- resources/views/components/category-any-tours.blade.php
+++ resources/views/components/category-any-tours.blade.php
@@ -1,4 +1,4 @@
-<section class="up_sale">
+<section class="up_sale new_year_section">
         <div class="container">
             <h2 class="h2_in_page">Смотрите так же <br> другие <span class="gold">туры:</span></h2>
 
