49b2732e3af5316ff1544c7078568088510f96a2
asmi046 <asmi046@gmail.com>
2025-10-12 20:05:58 +0300

    О туре

--- resources/views/tour_page.blade.php
+++ resources/views/tour_page.blade.php
@@ -2,6 +2,7 @@
 
 @section('main')
     <section class="banner_video">
+        <x-tours-arrow caption="О туре"></x-tours-arrow>
         <video autoplay muted loop playsinline preload="metadata"
         poster="{{ asset('img/poster.webp') }}"
         class="_video">
