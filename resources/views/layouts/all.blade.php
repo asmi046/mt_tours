8f70d98bbd94653a563477a36d41edc60227eedc
asmi046 <asmi046@gmail.com>
2026-01-05 13:46:42 +0300

    Замена в бегущей строке

--- resources/views/layouts/all.blade.php
+++ resources/views/layouts/all.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,41 +9,50 @@
 
     @header_seo
 
-    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon256.png')}}" sizes="256x256">
-    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon128.png')}}" sizes="128x128">
-    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon64.png')}}" sizes="64x64">
-    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon32.png')}}" sizes="32x32">
-    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon16.png')}}" sizes="16x16">
+    <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon256.png') }}" sizes="256x256">
+    <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon128.png') }}" sizes="128x128">
+    <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon64.png') }}" sizes="64x64">
+    <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon32.png') }}" sizes="32x32">
+    <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon16.png') }}" sizes="16x16">
 
     <meta name="_token" content="{{ csrf_token() }}">
-    <meta name="default_sale" content="{{ config('sales.default_sale') + (request()->is('tour/novogodniaia-adygeia') ? 500 : 0) }}">
+    <meta name="default_sale"
+        content="{{ config('sales.default_sale') + (request()->is('tour/novogodniaia-adygeia') ? 500 : 0) }}">
     <meta name="show_sale" content="{{ config('sales.show_sale') }}">
 
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
 
-    @vite([
-        'resources/css/app.css',
-        'resources/js/app.js',
-        'public/scss/main.scss'
-    ])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'public/scss/main.scss'])
 
     <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    <script type="text/javascript">
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
+                k, a)
+        })
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
         ym(100353854, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
         });
     </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/100353854" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/100353854" style="position:absolute; left:-9999px;" alt="" />
+        </div>
+    </noscript>
     <!-- /Yandex.Metrika counter -->
 
     <script>
@@ -52,30 +62,38 @@
     </script>
 
 </head>
+
 <body class="body @yield('dop_class')">
 
     <x-preloader></x-preloader>
 
     <x-menues.side-menu></x-menues.side-menu>
 
-    @include("allicon")
-    <main id="main"  class="main @yield('dop_class')">
+    @include('allicon')
+    <main id="main" class="main @yield('dop_class')">
         <div id="smooth-wrapper">
             <div id="smooth-content">
                 @if (config('sales.show_marquee'))
                     <section class="black_friday">
                         <div class="marquee">
                             <div class="marquee__content">
-                            @for ($i=0; $i<6; $i++)
-                                <p><a target="_blank" href="https://tours.mirturizma46.ru/tour/tur-elka-xxs">Рождественская Ёлка в ХХС 🎄</a></p>
-                                <img src="{{ asset('img/icon/snow_lent.svg') }}" alt="Снежинка">
-                                <p><a target="_blank" href="https://tours.mirturizma46.ru/tour/novogodniaia-belarus">Новогодняя Беларусь 2026 🎅</a></p>
-                                <img src="{{ asset('img/icon/snow_lent.svg') }}" alt="Снежинка">
-                                <p><a target="_blank" href="https://tours.mirturizma46.ru/tour/novogodniaia-adygeia">Новогодняя Адыгея 2026 ⛰️</a></p>
-                                <img src="{{ asset('img/icon/snow_lent.svg') }}" alt="Снежинка">
-                                <p><a target="_blank" href="https://tours.mirturizma46.ru/tour/novogodnee-ledovoe-sou-tatiany-navka">Новогоднее Ледовое Шоу ⛸️</a></p>
-                                <img src="{{ asset('img/icon/snow_lent.svg') }}" alt="Снежинка">
-                            @endfor
+                                @for ($i = 0; $i < 6; $i++)
+                                    <p><a target="_blank"
+                                            href="https://tours.mirturizma46.ru/tour/gastrotur-syr-xleb-i-vino">ГастроТур
+                                            «Кофе/Сыр. Хлеб и Вино» 🎄</a></p>
+                                    <img src="{{ asset('img/icon/snow_lent.svg') }}" alt="Снежинка">
+                                    <p><a target="_blank"
+                                            href="https://tours.mirturizma46.ru/tour/novogodniaia-belarus">Новогодняя
+                                            Беларусь 2026 🎅</a></p>
+                                    <img src="{{ asset('img/icon/snow_lent.svg') }}" alt="Снежинка">
+                                    <p><a target="_blank" href="https://tours.mirturizma46.ru/tour/optina">ОПТИНА
+                                            ПУСТЫНЬ + ШАМОРДИНО 🙏</a></p>
+                                    <img src="{{ asset('img/icon/snow_lent.svg') }}" alt="Снежинка">
+                                    <p><a target="_blank"
+                                            href="https://tours.mirturizma46.ru/tour/novogodnee-putesestvie-v-voronez">Альпаки
+                                            + Хаски + Океанариум 🐕</a></p>
+                                    <img src="{{ asset('img/icon/snow_lent.svg') }}" alt="Снежинка">
+                                @endfor
                             </div>
                         </div>
                     </section>
@@ -87,34 +105,35 @@
         </div>
 
     </main>
-            <div class="modal_win" id="global_app">
-                <modal-window rout="/send_consult" redirect="/thencs" hesh="showModal" title="Помощь специалиста" subtitle="Мы свяжемся с Вами в ближайшее время"></modal-window>
-                <empty-modal hesh="howpay" title="Как купить тур" subtitle="Инструкция по покупке тура">
-                    <div class="instruction_steps smooth-scroll " data-scrollsmoother="ignore">
-                        <div class="instruction_steps_wrap">
-                            <h2>Шаг №1</h2>
-                            <p>Выберите дату:</p>
-                            <img src="{{ asset('img/instruction/step1.webp') }}" alt="Выберите дату">
+    <div class="modal_win" id="global_app">
+        <modal-window rout="/send_consult" redirect="/thencs" hesh="showModal" title="Помощь специалиста"
+            subtitle="Мы свяжемся с Вами в ближайшее время"></modal-window>
+        <empty-modal hesh="howpay" title="Как купить тур" subtitle="Инструкция по покупке тура">
+            <div class="instruction_steps smooth-scroll " data-scrollsmoother="ignore">
+                <div class="instruction_steps_wrap">
+                    <h2>Шаг №1</h2>
+                    <p>Выберите дату:</p>
+                    <img src="{{ asset('img/instruction/step1.webp') }}" alt="Выберите дату">
 
-                            <h2>Шаг №2</h2>
-                            <p>Выберите тип тура:</p>
-                            <img src="{{ asset('img/instruction/step2.webp') }}" alt="Выберите тип тура">
+                    <h2>Шаг №2</h2>
+                    <p>Выберите тип тура:</p>
+                    <img src="{{ asset('img/instruction/step2.webp') }}" alt="Выберите тип тура">
 
-                            <h2>Шаг №3</h2>
-                            <p>Нажмите на кнопку для перехода к оплате:</p>
-                            <img src="{{ asset('img/instruction/step3.webp') }}" alt="Нажмите на кнопку для перехода к оплате">
+                    <h2>Шаг №3</h2>
+                    <p>Нажмите на кнопку для перехода к оплате:</p>
+                    <img src="{{ asset('img/instruction/step3.webp') }}" alt="Нажмите на кнопку для перехода к оплате">
 
-                            <h2>Шаг №4</h2>
-                            <p>Заполните персональные данные:</p>
-                            <img src="{{ asset('img/instruction/step4.webp') }}" alt="Заполните персональные данные">
+                    <h2>Шаг №4</h2>
+                    <p>Заполните персональные данные:</p>
+                    <img src="{{ asset('img/instruction/step4.webp') }}" alt="Заполните персональные данные">
 
-                            <h2>Шаг №5</h2>
-                            <p>Оплатите удобным способом:</p>
-                            <img src="{{ asset('img/instruction/step5.webp') }}" alt="Оплатите удобным способом">
-                        </div>
-                    </div>
-                </empty-modal>
+                    <h2>Шаг №5</h2>
+                    <p>Оплатите удобным способом:</p>
+                    <img src="{{ asset('img/instruction/step5.webp') }}" alt="Оплатите удобным способом">
+                </div>
             </div>
+        </empty-modal>
+    </div>
 
 </body>
 
