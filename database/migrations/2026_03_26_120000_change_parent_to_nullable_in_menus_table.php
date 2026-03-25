2ad5ea30cda35ec9fedac711db8c62dad8001bea
asmi046 <asmi046@gmail.com>
2026-03-26 00:40:24 +0300

    +Миграция

---
+++ database/migrations/2026_03_26_120000_change_parent_to_nullable_in_menus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            $table->integer('parent')
+                ->nullable()
+                ->default(null)
+                ->comment('Радительский пункт')
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            $table->integer('parent')
+                ->default(0)
+                ->nullable(false)
+                ->comment('Радительский пункт')
+                ->change();
+        });
+    }
+};
