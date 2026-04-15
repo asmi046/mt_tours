480e7f82bc1ba3cbcecd0345369bfff84cf98513
asmi046 <asmi046@gmail.com>
2026-04-16 02:20:56 +0300

    +1

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -125,7 +125,7 @@ class SitemapController extends Controller
                 'lastmod' => ($hotel->updated_at) ? $hotel->updated_at->format('Y-m-d\TH:i:s\Z') : Carbon::yesterday()->format('Y-m-d\TH:i:s\Z'),
                 'changefreq' => 'monthly',
                 'priority' => '0.6',
-                'image' => $hotel->img ? url('/storage/'.$hotel->img) : null,
+                'image' => $hotel->img ? url($hotel->img) : null,
             ]);
         });
 
