3b0ac17e3b828fec9b0548745e2a98fb94d5d9ba
asmi046 <asmi046@gmail.com>
2026-04-27 01:13:25 +0300

    +1

--- resources/views/zagran/page.blade.php
+++ resources/views/zagran/page.blade.php
@@ -45,6 +45,7 @@
     <section class="zagran_way_section_top">
         <div class="container">
             <h2>Самые выгодные предложения на туры в {{ $page->resort_name }}</h2>
+            <br>
             {!! $page->hot_tours_script !!}
 
             <div class="text_about_resort text_styles">
