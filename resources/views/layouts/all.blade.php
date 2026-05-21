cab5f059c2efad611a87440224b61a511f7da67b
asmi046 <asmi046@gmail.com>
2026-05-21 20:27:18 +0300

    cononical

--- resources/views/layouts/all.blade.php
+++ resources/views/layouts/all.blade.php
@@ -9,6 +9,8 @@
 
     @header_seo
 
+    <link rel="canonical" href="{{ url()->current() }}" />
+
     <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon256.png') }}" sizes="256x256">
     <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon128.png') }}" sizes="128x128">
     <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon64.png') }}" sizes="64x64">
