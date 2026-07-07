5eb96a62f1f0543647efc494f038036239e79b68
asmi046 <asmi046@gmail.com>
2026-07-07 17:21:05 +0300

    aria-label для ссылок

--- resources/views/components/social-net.blade.php
+++ resources/views/components/social-net.blade.php
@@ -1,6 +1,6 @@
 <div class="social_net">
-    <a href="{{ config('contacts.vk') }}" class="social_lnk sl_vk"></a>
-    <a href="{{ config('contacts.ok') }}" class="social_lnk sl_ok"></a>
-    <a href="{{ config('contacts.tg') }}" class="social_lnk sl_tg"></a>
-    <a href="{{ config('contacts.ws') }}" class="social_lnk sl_ws"></a>
+    <a href="{{ config('contacts.vk') }}" aria-label="Наша группа ВКонтакте" class="social_lnk sl_vk"></a>
+    <a href="{{ config('contacts.ok') }}" aria-label="Наша группа Одноклассники" class="social_lnk sl_ok"></a>
+    <a href="{{ config('contacts.tg') }}" aria-label="Напиши в Telegram" class="social_lnk sl_tg"></a>
+    <a href="{{ config('contacts.ws') }}" aria-label="Наш чат WhatsApp" class="social_lnk sl_ws"></a>
 </div>
