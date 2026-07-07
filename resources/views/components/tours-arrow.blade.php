ab835d24e68c5be05dc9e451d9f26779e3792d59
asmi046 <asmi046@gmail.com>
2026-07-07 18:49:13 +0300

    Еще одна ссылка

--- resources/views/components/tours-arrow.blade.php
+++ resources/views/components/tours-arrow.blade.php
@@ -3,10 +3,10 @@
     'link' => 'tours_cat',
 ])
 <div class="more_tours_caption">
-            {{ $caption }}
-        </div>
-        <a href="#{{ $link }}" class="more_tours yakor">
-            <svg class="sprite_icon">
-                <use xlink:href="#arrow"></use>
-            </svg>
-        </a>
+    {{ $caption }}
+</div>
+<a href="#{{ $link }}" aria-label="{{ $caption }}" class="more_tours yakor">
+    <svg class="sprite_icon">
+        <use xlink:href="#arrow"></use>
+    </svg>
+</a>
