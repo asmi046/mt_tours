a469175cbf09f50425aa57818931785b71140e96
asmi046 <asmi046@gmail.com>
2026-07-28 17:55:12 +0300

    Август

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -58,7 +58,7 @@ class CalendarController extends Controller
 
     public function index($month = null)
     {
-        $curent_month = $month ? $month : date('d.m.Y', strtotime('01.07.2026'));
+        $curent_month = $month ? $month : date('d.m.Y', strtotime('01.08.2026'));
 
         $src_event = Calendar::all();
         $events = [];
