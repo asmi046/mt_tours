5a529b3880c5cecc75fd70906502dc3cb4e1b328
asmi046 <asmi046@gmail.com>
2026-08-28 16:23:32 +0300

    +1

--- resources/views/components/presentation-widget.blade.php
+++ resources/views/components/presentation-widget.blade.php
@@ -2,7 +2,7 @@
 
 <div class="presentation_widget" data-presentation-widget hidden>
     <button class="presentation_widget_close" type="button" aria-label="Закрыть" data-presentation-close>
-        <svg viewBox="0 0 24 24" width="14" height="14" fill="none" stroke="currentColor" stroke-width="2.5"
+        <svg viewBox="0 0 24 24" width="12" height="12" fill="none" stroke="currentColor" stroke-width="3"
             stroke-linecap="round">
             <line x1="6" y1="6" x2="18" y2="18"></line>
             <line x1="18" y1="6" x2="6" y2="18"></line>
