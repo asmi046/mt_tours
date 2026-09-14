6e274c8cee633ec5fc7f991c184b28c0c637cf6c
asmi046 <asmi046@gmail.com>
2026-09-14 19:16:37 +0300

    Убрал ссылку

--- resources/views/components/cards/school.blade.php
+++ resources/views/components/cards/school.blade.php
@@ -1,8 +1,8 @@
 <div class="school_card">
     @if ($item->img)
-        <a href="{{ Storage::url($item->img) }}" data-caption="{{ $item->title }}" data-fslightbox="page-galery" class="school_card_img">
-            <img src="{{ Storage::url($item->img) }}" alt="{{ $item->title }}">
-        </a>
+        {{-- <a href="{{ Storage::url($item->img) }}" data-caption="{{ $item->title }}" data-fslightbox="page-galery" class="school_card_img"> --}}
+        <img src="{{ Storage::url($item->img) }}" alt="{{ $item->title }}">
+        {{-- </a> --}}
     @endif
     <div class="school_card_content">
         <h2 class="school_card_title">{!! $item->title !!}</h2>
