3b93c7f114d8d7502fa87e9d1a954b2327f5bdbf
asmi046 <asmi046@gmail.com>
2026-09-15 22:21:14 +0300

    +1

--- resources/views/layouts/all.blade.php
+++ resources/views/layouts/all.blade.php
@@ -106,9 +106,8 @@
                                     {{-- <img src="{{ asset('img/icon/flovers.svg') }}" alt="Снежинка"> --}}
                                     <p><a target="_blank" href="https://www.mirturizma46.ru/tour/sankt-peterburg">Питер
                                             + Корелия + Петергоф (23 сентября) ⛲</a></p>
-                                    <p><a target="_blank" href="https://www.mirturizma46.ru/tour/adygea">Адыгея сентябрь
-                                            -
-                                            2026 ⛰️</a></p>
+                                    <p><a target="_blank" href="https://www.mirturizma46.ru/tour/adygea">Адыгея (24
+                                            сентября) ⛰️</a></p>
                                     {{-- <img src="{{ asset('img/icon/flovers.svg') }}" alt="Снежинка"> --}}
                                 @endfor
                             </div>
